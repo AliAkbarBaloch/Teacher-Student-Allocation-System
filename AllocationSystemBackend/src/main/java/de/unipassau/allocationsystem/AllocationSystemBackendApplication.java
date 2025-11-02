f09907ed4c8008c3cc21c7fe5f1e11c296b2520c
Mustafa Poya <mustafapoyaabc@gmail.com>
2025-11-02 18:27:25 +0100

    init the project backend and frontend
    - create the Main template of backend and frontend
    - create documentation and command files for future use
    - create data folder for adding other related documents and reports
    -

---
+++ AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/AllocationSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package de.unipassau.allocationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllocationSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllocationSystemBackendApplication.class, args);
+    }
+
+}
---
+++ AllocationSystemBackend/src/test/java/de/unipassau/allocationsystem/AllocationSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unipassau.allocationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllocationSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
