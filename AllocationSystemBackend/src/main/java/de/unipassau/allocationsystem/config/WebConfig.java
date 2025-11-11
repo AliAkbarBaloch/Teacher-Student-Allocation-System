82aa6172334f2c19ada91d77fcd7fed1ffa787a3
Mustafa Poya <mustafapoyaabc@gmail.com>
2025-11-11 22:31:48 +0100

    fix pipe issues in frontend and backend
    - remove conflict of pckages in package.json
    - fix issues inside the application.proprties.
    - remove deprecated codes from Security Config
    - fix package name inside the web config

--- AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/config/WebConfig.java
+++ AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.ecommerce.config;
+package de.unipassau.allocationsystem.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
