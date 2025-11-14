99d0e1c793e73468b4d837ee1a6a414efeaf2dac
Mustafa Poya <mustafapoyaabc@gmail.com>
2025-11-14 23:39:49 +0100

    change App folder to app

--- AllocationSystemFrontend/src/App/main.tsx
+++ AllocationSystemFrontend/src/App/main.tsx
@@ -1,11 +1,9 @@
-import '../i18n'; // run i18n setup (index.ts) once at startup
+import '../i18n/index.ts'; // run i18n setup (index.ts) once at startup
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- AllocationSystemFrontend/src/app/App.tsx
+++ AllocationSystemFrontend/src/app/App.tsx
@@ -1,7 +1,6 @@
 import {useTranslation} from "react-i18next"; // import translation hook
 
 function App() {
-
   // call the hook 
   const {t} = useTranslation();
 
