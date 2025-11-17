ab291a2e3b463b7cb9d1e17c872ebe13f42d15a0
Mustafa Poya <mustafapoyaabc@gmail.com>
2025-11-17 10:26:11 +0100

    Remove unused import in InternshipTypeService
    Deleted the unused import of org.springframework.beans.factory.annotation.Autowired from InternshipTypeService.java to clean up the code.

--- AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/service/InternshipTypeService.java
+++ AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/service/InternshipTypeService.java
@@ -8,7 +8,6 @@ import de.unipassau.allocationsystem.utils.PaginationUtils;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
