3afdcdc940f5fe871f0aaca3b15872ca31d8b509
Mustafa Poya <mustafapoyaabc@gmail.com>
2025-11-18 09:21:15 +0100

    Add new permission constants for base data
    Introduced permission constants for academic year, audit log, internship type, and subject category to improve granularity in access control. Grouped permissions into user, base data, and report categories for better organization.

--- AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/constant/PermissionConstants.java
+++ AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/constant/PermissionConstants.java
@@ -3,17 +3,25 @@ package de.unipassau.allocationsystem.constant;
 public class PermissionConstants {
 
     public static class Permissions {
+        // user
         public static final String PERMISSION = "permission";
         public static final String ROLE = "role";
         public static final String ROLE_PERMISSION = "rolePermission";
         public static final String USER = "user";
-        public static final String DASHBOARD = "dashboard";
-        public static final String REPORT = "report";
+
+        // base data
+        public static final String ACADEMIC_YEAR = "academicYear";
+        public static final String AUDIT_LOG = "auditLog";
+        public static final String INTERNSHIP_TYPE = "internshipType";
         public static final String SCHOOL = "school";
+        public static final String SUBJECT_CATEGORY = "subjectCategory";
         public static final String TEACHER = "teacher";
         public static final String TEACHER_AVAILABILITY = "teacherAvailability";
-        public static final String INTERNSHIP_TYPE = "internshipType";
         public static final String TEACHER_FORM_SUBMISSION = "teacherFormSubmission";
+
+        // Report
+        public static final String DASHBOARD = "dashboard";
+        public static final String REPORT = "report";
     }
 
     public static class AccessLevels {
