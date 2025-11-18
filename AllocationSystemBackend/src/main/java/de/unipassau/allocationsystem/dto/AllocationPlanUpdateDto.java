77c93ef25bb065f9c882a5bebbf8d8db30fa9da5
Ali Akbar <akbar01@ads.uni-passau.de>
2025-11-18 16:53:52 +0100

    Added AllocationPlan CRUD

---
+++ AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/dto/AllocationPlanUpdateDto.java
@@ -0,0 +1,27 @@
+package de.unipassau.allocationsystem.dto;
+
+import de.unipassau.allocationsystem.entity.AllocationPlan.PlanStatus;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO for updating an existing allocation plan.
+ * All fields are optional to support partial updates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AllocationPlanUpdateDto {
+
+    @Size(max = 255, message = "Plan name must not exceed 255 characters")
+    private String planName;
+
+    private PlanStatus status;
+
+    private Boolean isCurrent;
+
+    @Size(max = 5000, message = "Notes must not exceed 5000 characters")
+    private String notes;
+}
