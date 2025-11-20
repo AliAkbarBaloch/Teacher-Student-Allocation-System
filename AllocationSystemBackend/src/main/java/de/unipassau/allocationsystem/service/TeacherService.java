99c589d943946b2cba3bed27b305de3f42872bb5
Maisam Afshar <maisam.afshar@gmail.com>
2025-11-20 11:31:40 +0100

    refactor: remove unused imports from TeacherRepository and TeacherService
    - Clean up code by eliminating unnecessary imports to enhance readability and maintainability.

--- AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/service/TeacherService.java
+++ AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/service/TeacherService.java
@@ -2,7 +2,6 @@ package de.unipassau.allocationsystem.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
