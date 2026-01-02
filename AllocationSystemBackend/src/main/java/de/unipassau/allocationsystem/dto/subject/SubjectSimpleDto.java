efc2942293bd9b7dfed275b024f59323db9a5739
Lena <petukh01@ads.uni-passau.de>
2026-01-02 16:06:15 +0100

    JavaDoc for SubjectSimpleDto

--- AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/dto/subject/SubjectSimpleDto.java
+++ AllocationSystemBackend/src/main/java/de/unipassau/allocationsystem/dto/subject/SubjectSimpleDto.java
@@ -5,8 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter; 
 
-//minimal subject DTO used inside other responses 
-
+/**
+ * Minimal subject DTO used inside other responses.
+ *
+ * <p>
+ * This DTO contains only basic subject information and is intended to be embedded
+ * in other DTOs where full subject details are not required.
+ * </p>
+ */
 @Getter
 @Setter
 @NoArgsConstructor
